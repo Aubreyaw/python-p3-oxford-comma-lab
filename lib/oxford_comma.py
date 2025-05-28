4274b3e7bc976f89ef96321b4894857e06bb760d
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-28 12:42:25 -0400

    Completes lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,23 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+       return " and ".join(items)
+    elif len(items) == 3:
+        all_but_last = items[:-1]
+        last_item = items[-1]
+        joined = ", ".join(all_but_last)
+        final_string = joined + ", and " + last_item
+        return final_string
+    elif len(items) > 3:
+        all_but_last = items[:-1]
+        last_item = items[-1]
+        joined = ", ".join(all_but_last)
+        final_string = joined + ", and " + last_item
+        return final_string
+        
+ 
+        
+
+
+    
